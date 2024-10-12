d0c98bd630f87ef9f0de427f466c0d380cea195f
Maksim T <tkachenkollmaksim@gmail.com>
2024-10-11 18:15:45 -0700

    AddedFile

---
+++ features/tests/help_tests.feature
@@ -0,0 +1,6 @@
+# Created by maksimtkachenko at 10/7/24
+Feature: # Enter feature name here
+  # Enter feature description here
+
+  Scenario: # Enter scenario name here
+    # Enter steps here
---
+++ pages/help_page.py
@@ -0,0 +1,11 @@
+from behave import given, when, then
+
+
+@given('Open Help page for Returns')
+def click_cart(context):
+    context.app.help_page.open_help_returns()
+
+
+@when('Select Help topic {option}')
+def select_topic(context, option):
+    context.app.help_page.select_topic(option)
